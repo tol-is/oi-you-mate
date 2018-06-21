4ca4b6d27dd38eccdd6a01196a9f364fa656a692
Apostolos Christodoulou <me@tol.is>
2018-06-21 11:59:00 +0100

    changes footer link to me

--- src/sections/footer/index.js
+++ src/sections/footer/index.js
@@ -11,7 +11,7 @@ export default () => {
         <p className={style.footer_header}>It ain't<br/>over<br/>till the<br/>fat lady<br/>sings</p>
         <p>
         Body type is set in <strong><a target="_blank" href="https://www.myfonts.com/fonts/intelligent-foundry/averta/">Averta</a></strong><br/>
-        Html by <strong><a  target="_blank" href="https://github.com/ap-o">ap-o</a></strong><br/>
+        Html by <strong><a  target="_blank" href="https://github.com/tol-is">tol-is</a></strong><br/>
         <strong>Special thanks to <a target="_blank"href="http://www.gerardunger.com/">Dr. Gerard Unger</a> and <a target="_blank" href="http://www.shaniavni.com/">Shani Avni.</a></strong></p>
         <p><a  target="_blank"href="https://kostasbartsokas.com/">Kostas Bartsokas</a> © 2017</p>
       </div>
